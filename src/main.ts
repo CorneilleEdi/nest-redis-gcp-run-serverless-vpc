fead6fa47e9210cafb1e31a7f77a93f6e2c27ca4
CorneilleEdi <corneilleayao@gmail.com>
2021-04-21 08:14:36 +0530

    feat: add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CONFIGURATIONS } from './shared/configuration';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    app.enableCors();
+
+    // Get services
+    const configService = app.get(ConfigService);
+
+    const PORT = configService.get(CONFIGURATIONS.PORT);
+
+    await app.listen(PORT, async () => {
+        new Logger('Application').verbose(`Server started ,running and listening on port ${PORT}`);
+    });
 }
 bootstrap();
